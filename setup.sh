fee2bb554018881024fa572ed8e45b4511b52169
Michael Ball <mball@lmoiseenko-ltr.internal.salesforce.com>
2013-05-29 17:32:47 -0700

    some initial work

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+# New Mac Config
+#########################
+
+# Finder
+
+# Dock
+
+# Time Machine
+
+# Terminal
+
+# Universal access
+
+# Homebrew
+ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+brew install wget
