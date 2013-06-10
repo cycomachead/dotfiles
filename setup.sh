bf76306df7bdfbb4e9751c4d8874119d5e154e59
Michael Ball <cycomachead@gmail.com>
2013-06-10 01:31:16 -0700

    Fixes. Copying and permissions should be set.

--- setup.sh
+++ setup.sh
@@ -16,12 +16,13 @@ mkdir setup && cd setup
 FILE='{.aliases,.bash,.bash_profile,.bash_prompt,.bashrc,.exports,.extra,.functions,.gitattributes,.gitconfig,.gitignore,.osx,.tm_properties,.zsh,Michael.terminal}'
 curl -# ${URL}/${FILE} -o "#1"
 
+cd ~/Desktop/setup/
 # Copy Source files.
-cp -f .bash_profile ${DEST}/.bash_profile
-cp -f .bashrc ${DEST}/.bashrc
-cp -f .gitconfig ${DEST}/.gitconfig
-cp -f .gitignore ${DEST}/.gitignore
-cp -f .tm_properties ${DEST}/.tm_properties
+cp -f .bash_profile "${DEST}/.bash_profile"
+cp -f .bashrc "${DEST}/.bashrc"
+cp -f .gitconfig "${DEST}/.gitconfig"
+cp -f .gitignore "${DEST}/.gitignore"
+cp -f .tm_properties "${DEST}/.tm_properties"
 
 
 # Break to install Xcode and CL tools
