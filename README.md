b8d36b530fd505ea35eb59f23c9bf41b881ff41c
Michael Ball <cycomachead@gmail.com>
2013-06-10 01:04:25 -0700

    Added a bunch of work

---
+++ README.md
@@ -0,0 +1,10 @@
+# dotfiles
+
+My simple collection of setup files for my mac. These haven't all been tested and are not guaranteed to work.
+There's also quite a few settings specific to my use case (extra large cursor and fonts), which others might not want.
+
+Many things are a mash-up from other repo's I've found.
+
+## License
+
+BSD, 3 clause license, w/o any warranty.
--- setup.sh
+++ setup.sh
@@ -1,18 +1,39 @@
-#! /bin/sh
+ #! /bin/sh
 
 # New Mac Config
 #########################
+DEST='/Volumes/Macintosh HD/Users/Michael/'
 
-# Finder
+sudo -v
 
-# Dock
+# Setup Users Dir
+# dscl . Michael -change dsAttrTypeNative:home /Users/Michael ${DEST}
 
-# Time Machine
+# Grab the full GH repo and cd into it.
+URL='https://raw.github.com/cycomachead/dotfiles/master'
+cd ~/Desktop
+mkdir setup && cd setup
+FILE='{.aliases,.bash,.bash_profile,.bash_prompt,.bashrc,.exports,.extra,.functions,.gitattributes,.gitconfig,.gitignore,.osx,.tm_properties,.zsh,Michael.terminal}'
+curl -# ${URL}/${FILE} -o "#1"
 
-# Terminal
+# Copy Source files.
+for file in ./.{bash_profile,bashrc,gitconfig,gitignore,tm_properties}
+do
+    cp ${file} ${DEST}${file}
+done
 
-# Universal access
+# Break to install Xcode and CL tools
+read -p "Delaying to Install Apps. " -n 1
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	echo "Continuing."
+fi
+
+# Disable auto brightness settings
 
 # Homebrew
-ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
-brew install wget
+# ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+# brew install wget
+
+# Configure Defaults
+./.osx
