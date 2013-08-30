d3fdbbd99676c47bc399247fdff8f8c3593a37a7
Michael Ball <cycomachead@gmail.com>
2013-08-30 12:46:41 -0700

    Cleaned up aliases and class fn's. Moved old stuff to new 'previous' file

--- .bash
+++ .bash
@@ -1,13 +1,6 @@
-
-# work when piping: ls | less
-alias lsl="ls -FlaG"
-
-# some cd aliases
-alias cd.='cd .'
-alias cd..='cd ..'
-
 # Defined Variables:
 # =====================================
+sync="rsync -avz"
 ser="cs.berkeley.edu"
 cs10="cs10@cory.$ser" # CS10 Master Account
 cs10t="cs10-th@cory.$ser"
@@ -27,9 +20,8 @@ alias makw="make" #fix for common typos
 alias matw="mate"
 alias cddot="cd ${dotf}"
 alias mate.="mate . * */*"
-alias cdd="cd ~/Dropbox/"
 alias cdc="cd ~/Dropbox/College/$curr"
-alias jekylls="jekyll --config _config-dev.yml"
+alias jekylls="jekyll serve --watch"
 alias gc="git commit -am"
 alias gcm="git commit"
 alias gp="git push"
@@ -38,56 +30,16 @@ alias push-post="~/Dropbox/Projects/Website/tools/push-post.sh"
 alias apsync="~/Dropbox/Projects/ApertureSync/sync.sh"
 
 # CLASSES
-# CS10
-alias cd10="cd ~/Dropbox/College/Sp13/CS10"
+# =======
+# CS10 Fa13 (updated)
+alias cd10="cd ~/Dropbox/College/$cur/CS10"
 alias ssh10="ssh $cs10"
-alias cs10="open -a Safari http://inst.eecs.berkeley.edu/~cs10/"
-alias scp10="scp -r ../ $cs10"
-alias pull10="scp -r $cs10:~/* ~/Documents/CS10/"
-# CS61C
-alias cd61="cd ~/Dropbox/College/Sp13/CS61C/61c"
-alias ssh61="ssh cs61c-mx@hive16.cs.berkeley.edu"
-alias 61c="open -a Safari http://inst.eecs.berkeley.edu/~cs61c/"
-alias scp70="scp -r ../ cs61c-mx@nova.cs.berkeley.edu"
-alias matel="mate ~/Dropbox/College/Sp13/CS61C/61c/labs/*"
-alias mateh="mate ~/Dropbox/College/Sp13/CS61C/61c/homework/*"
-# CS70
-alias cd70="cd ~/Dropbox/College/Sp13/CS70"
-alias ssh70="ssh cs70-ha@torus.cs.berkeley.edu"
-alias cs70="open -a Safari http://inst.eecs.berkeley.edu/~cs70/"
-alias scp70="scp -r * cs70-ha@nova.cs.berkeley.edu:~/homework/"
-function push-70 {
-    temp=$PWD
-    cdcol
-    cd Sp13/CS70/homework/
-    rsync -ahvz --exclude="problems" ./ ${cs70}:~/homework/
-    cd "$temp"
-    unset temp
-}
+alias cs10="open -a Safari http://inst.eecs.berkeley.edu/~cs10/$cur"
+alias push10="$sync ../ $cs10"
+alias pull10="$sync $cs10:~/* ~/Documents/CS10/"
 
-# OLD 61B ================================
-# alias hw="~/Dropbox/College/Fa12/CS61B/support/hw"
-# A specific mate command modified for each class project.
-# alias mate..="make clean; mate . loa/*.java"
-# alias hwc="make clean; hw commit"
-# alias hws="make clean; hw submit"
-
-# CS194
-# Master account.
-cs194="cs194-23@torus.cs.berkeley.edu"
-alias cs194="open -a Safari http://inst.eecs.berkeley.edu/~cs194-23/"
-alias cd194="cd ~/Dropbox/College/Sp13/CS194-23"
-alias ssh194="ssh $cs194"
-function push_194 {
-    temp=$PWD
-    cdcol
-    cd Sp13/CS194-23/cs194-site/
-        rsync -avz $1/ ${cs194}:~/public_html/$1/
-    cd "$temp"
-    unset temp
-}
-# Update this for each semester.
-alias push-194="push_194 sp13"
+# EE 122 Fa13
+# EE 42 Fa13
 
 # EXPORTS:
 # =================================
@@ -95,11 +47,10 @@ export RMATE_HOST=auto
 export RMATE_PORT=12345
 # this needs to be fixed for editing crontabs:
 export EDITOR="/usr/local/bin/mate -w"
-# For CS61B, probably doesn't work well anymore.
-# export CLASSPATH=/lib/junit.jar:/lib/ucb.jar:
 export TM_SUPPORT_PATH='/Applications/TextMate.app/Contents/SharedSupport/Support'
 
 # Temporary Fix for the above...
+# Use cront to edit crontabs.
 function cront
 {
     unset EDITOR
---
+++ previous.sh
@@ -0,0 +1,48 @@
+# Previous Semester Settings and alaises to be used for reference
+# CS61C
+alias cd61="cd ~/Dropbox/College/Sp13/CS61C/61c"
+alias ssh61="ssh cs61c-mx@hive16.cs.berkeley.edu"
+alias 61c="open -a Safari http://inst.eecs.berkeley.edu/~cs61c/"
+alias scp70="scp -r ../ cs61c-mx@nova.cs.berkeley.edu"
+alias matel="mate ~/Dropbox/College/Sp13/CS61C/61c/labs/*"
+alias mateh="mate ~/Dropbox/College/Sp13/CS61C/61c/homework/*"
+# CS70
+alias cd70="cd ~/Dropbox/College/Sp13/CS70"
+alias ssh70="ssh cs70-ha@torus.cs.berkeley.edu"
+alias cs70="open -a Safari http://inst.eecs.berkeley.edu/~cs70/"
+alias scp70="scp -r * cs70-ha@nova.cs.berkeley.edu:~/homework/"
+function push-70 {
+    temp=$PWD
+    cdcol
+    cd Sp13/CS70/homework/
+    rsync -ahvz --exclude="problems" ./ ${cs70}:~/homework/
+    cd "$temp"
+    unset temp
+}
+
+# OLD 61B ================================
+# alias hw="~/Dropbox/College/Fa12/CS61B/support/hw"
+# A specific mate command modified for each class project.
+# alias mate..="make clean; mate . loa/*.java"
+# alias hwc="make clean; hw commit"
+# alias hws="make clean; hw submit"
+
+# CS194
+# Master account.
+cs194="cs194-23@torus.cs.berkeley.edu"
+alias cs194="open -a Safari http://inst.eecs.berkeley.edu/~cs194-23/"
+alias cd194="cd ~/Dropbox/College/Sp13/CS194-23"
+alias ssh194="ssh $cs194"
+function push_194 {
+    temp=$PWD
+    cdcol
+    cd Sp13/CS194-23/cs194-site/
+        rsync -avz $1/ ${cs194}:~/public_html/$1/
+    cd "$temp"
+    unset temp
+}
+# Update this for each semester.
+alias push-194="push_194 sp13"
+
+# For CS61B, probably doesn't work well anymore.
+export CLASSPATH=/lib/junit.jar:/lib/ucb.jar:
