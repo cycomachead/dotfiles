f9a92894730ccbd5f5add10c76810406d773ed28
Michael Ball <cycomachead@gmail.com>
2013-08-31 16:11:12 -0700

    tweak website shortcuts

--- .bash
+++ .bash
@@ -1,6 +1,6 @@
 # Defined Variables:
 # =====================================
-sync="rsync -avz"
+sync="rsync -avz --delete"
 ser="cs.berkeley.edu"
 cs10="cs10@cory.$ser" # CS10 Master Account
 cs10t="cs10-th@cory.$ser"
@@ -32,11 +32,14 @@ alias apsync="~/Dropbox/Projects/ApertureSync/sync.sh"
 # CLASSES
 # =======
 # CS10 Fa13 (updated)
+10master="~/Documents/CS10/"
 alias cd10="cd ~/Dropbox/College/$cur/CS10"
 alias ssh10="ssh $cs10"
 alias cs10="open -a Safari http://inst.eecs.berkeley.edu/~cs10/$cur"
-alias push10="$sync ../ $cs10"
-alias pull10="$sync $cs10:~/* ~/Documents/CS10/"
+alias push10="$sync ${10master} $cs10"
+alias pull10="$sync $cs10:~/* ${10master}"
+alias pushsite="$sync ${10master}/public_html/* $cs10/public_html/"
+alias pullsite="$sync $cs10:~/pulic_html/* ${10master}/public_html/"
 
 # EE 122 Fa13
 # EE 42 Fa13
