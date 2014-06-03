a4a6a2f0b05af015df148a0611da9997f583a813
Michael Ball <cycomachead@gmail.com>
2014-06-02 23:57:53 -0700

    minor tweaks to install script

--- setup.sh
+++ setup.sh
@@ -63,6 +63,7 @@ brew install wget exiv2 gphoto2 python3 node hg hub
 npm install http-server -g
 npm install jshint -g
 npm install jslint -g
+npm install -g localtunnel
 sudo easy_install pip # may not be necessary in the future...
 # brew install ruby -- no longer necessary in Mavericks
 # Update gems and install jekyll
