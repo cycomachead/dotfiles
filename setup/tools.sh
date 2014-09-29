5a080575f36cd64bda5ff7cba32816c02e96d9b8
Michael Ball <cycomachead@gmail.com>
2014-09-28 17:51:48 -0700

    some tweaks and a new brew install location

--- setup/tools.sh
+++ setup/tools.sh
@@ -1,9 +1,10 @@
+sudo -v
 # Install the hk client for heroku
 # source github/heroku/hk (This is beta and may change!)
 L=/usr/local/bin/hk && curl -sL -A "`uname -sp`" https://hk.heroku.com/hk.gz | zcat >$L && chmod +x $L
 
 # Homebrew and tools.
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor
 brew install git wget exiv2 gphoto2 python3 node hg hub bfg heroku
 
