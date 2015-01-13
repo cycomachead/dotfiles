f4479b780b167b8eeadf9cdec40ac3e6d60cd933
Michael Ball <cycomachead@gmail.com>
2015-01-12 22:08:28 -0800

    update some tools and install prefs

--- setup/tools.sh
+++ setup/tools.sh
@@ -1,4 +1,5 @@
 sudo -v
+
 # Install the hk client for heroku
 # source github/heroku/hk (This is beta and may change!)
 L=/usr/local/bin/hk && curl -sL -A "`uname -sp`" https://hk.heroku.com/hk.gz | zcat >$L && chmod +x $L
@@ -6,18 +7,20 @@ L=/usr/local/bin/hk && curl -sL -A "`uname -sp`" https://hk.heroku.com/hk.gz | z
 # Homebrew and tools.
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor
-brew install git wget exiv2 gphoto2 python3 node hg hub bfg heroku
+brew install git wget exiv2 gphoto2 python3 node hg hub bfg heroku ghi
 
-npm install -g localtunnel psi jshint grunt-cli coffee-script
+npm install -g localtunnel psi jshint grunt-cli coffee-script bower
 # Hubot tools, probably don't need to be global
 # hubot hubot-hipchat
+
+# My Apps Brew-Casks
+
 
 
 # Pip for default python
 sudo easy_install pip
 pip install virtualenv
 
-# brew install ruby -- no longer necessary in Mavericks
 # Update gems and install jekyll
-sudo gem install jekyll
+sudo gem install jekyll bundle
 sudo gem update --system
