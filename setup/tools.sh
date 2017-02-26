144d9a93690225948627354754c238801e7fd9b2
Michael Ball <cycomachead@gmail.com>
2017-02-26 15:20:07 -0800

    Update tools

--- setup/tools.sh
+++ setup/tools.sh
@@ -1,27 +1,25 @@
 sudo -v
 
-# Install the hk client for heroku
-# source github/heroku/hk (This is beta and may change!)
-L=/usr/local/bin/hk && curl -sL -A "`uname -sp`" https://hk.heroku.com/hk.gz | zcat >$L && chmod +x $L
-
 # Homebrew and tools.
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor
-brew install git exiv2 gphoto2 python3 node hub
+brew install heroku git exiv2 gphoto2 python3 node hub postgres-9.5
 
-npm install -g localtunnel psi jshint grunt-cli coffee-script bower
+npm install -g psi jshint grunt-cli coffee-script bower
 
 # My Apps Brew-Casks
 brew install caskroom/cask/brew-cask
-# brew cask install google-chrome
+brew cask install google-chrome
 brew cask install carbon-copy-cloner
 brew cask install hipchat
-brew cask install skype
-brew cask install google-earth
-brew cask install transmission
+# brew cask install skype
+# brew cask install google-earth
+# brew cask install transmission
 brew cask install textmate
-brew cask install sourcetree
-
+# brew cask install sourcetree
+# Gradescope Tools
+brew cask install front
+brew cask install vagrant
 
 
 # Pip for default python
@@ -29,10 +27,11 @@ sudo easy_install pip
 pip install virtualenv
 
 # Setup Ruby
+# TODO: This is rather dangerous. :(
 curl -sSL https://get.rvm.io | bash -s stable
 
 rvm install 2.3.1
-rvm install 1.9.3 # legacy projects
-rvm use 2.2.0
+rvm install 2.4.0
+rvm use stable
 gem install jekyll bundler
 sudo gem update --system
