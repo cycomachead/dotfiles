dcf839bce7f29ffa7ad8aef6d0858db1e433206d
Michael Ball <cycomachead@gmail.com>
2018-11-06 16:55:41 -0800

    Create a new symlink file

---
+++ setup/link_home_files.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+dotfiles='~/Documents/Projects/dotfiles/home/'
+
+function symlink {
+    rm $1;
+    ln -s $dotfiles/$1 $1;
+}
+
+pushd ~/
+symlink .bash_profile
+symlink .bashrc
+symlink .tm_properties
+symlink .inputrc
