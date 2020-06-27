08c4ff2333bc391bfc5c88441a922d3553b4bbec
Michael Ball <cycomachead@gmail.com>
2020-06-27 01:40:40 -0700

    Cleanup and fix setup / linking script

--- setup/link_home_files.sh
+++ setup/link_home_files.sh
@@ -1,16 +1,16 @@
 #! /usr/bin/env bash
 
-dotfiles='~/Dropbox/Projects/dotfiles/home/'
+user='/Volumes/Michael/Users/michael/'
+dotfiles="$user/Dropbox/Projects/dotfiles/home/"
 
 function symlink {
-    rm $1;
-    ln -s $dotfiles/$1 $1;
+    rm .$1 || true;
+    ln -s $dotfiles$1 .$1;
 }
 
 pushd ~/
-symlink .bash_profile
-symlink .bashrc
-symlink .tm_properties
-symlink .inputrc
-symlink .psqlrc
+for file in $(ls $dotfiles)
+do
+    symlink $file
+done
 popd
