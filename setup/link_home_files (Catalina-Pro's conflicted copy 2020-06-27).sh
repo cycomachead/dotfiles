3b3c94ff4d450bd288198c70c3e8d0e26703b6ca
Michael Ball <cycomachead@gmail.com>
2020-06-29 18:25:26 -0700

    Do not use hidden files in the dotfiles repo

--- setup/link_home_files (Catalina-Pro's conflicted copy 2020-06-27).sh
+++ setup/link_home_files (Catalina-Pro's conflicted copy 2020-06-27).sh
@@ -1,16 +1,16 @@
 #! /usr/bin/env bash
 
-dotfiles="$HOME/Dropbox/Projects/dotfiles/home/"
+user='/Volumes/Michael/Users/michael/'
+dotfiles="$user/Dropbox/Projects/dotfiles/home/"
 
 function symlink {
-    echo "Linking $1"
-    rm .$1;
+    rm .$1 || true;
     ln -s $dotfiles$1 .$1;
 }
 
-pushd ~/;
+pushd ~/
 for file in $(ls $dotfiles)
 do
     symlink $file
 done
-popd;
+popd
