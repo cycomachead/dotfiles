348283e9ec879b7c9bf911fa5c3755b2a31b4419
Michael Ball <cycomachead@gmail.com>
2020-06-30 17:28:51 -0700

    A bunch of work with defaults write

---
+++ setup/README.md
@@ -0,0 +1,346 @@
+# macOS Preferences
+
+This is a fairly opinionated (and increasingly comprehensive) set of
+macOS preferences, based on my daily use.
+
+This README documents what high level changes I've made for applications. The specifics are in the plist files.
+
+## A Note on `defaults`
+
+My dotfiles repos use individual shell commands. I am chosing to make use of recent macOS's nicer key-value plist format and the `import` command to manage default settings.
+
+### Export current settings:
+
+For example, to dump Finder prefs:
+
+```sh
+defaults read com.apple.Finder > setup/app-preferences/finder.plist
+```
+*Note:* After dumping a set of preferences, you'll likely want to clean up the local cruft, such as things that include timestamps or notes about migrated data.
+
+I typically start of with a dump, because it shows me the various newer settings that I have tweaked.
+
+### Import Preferences:
+
+For example, using the `-app` shortcut to reference an application, which is not necessary, but sometimes handy.
+
+```sh
+defualts import -app setup/app-preferences/safari.plist
+```
+
+## Universal Access
+
+If you're not me and cloning this, then you should know I have some very specific UA settings in here. These are necessary for me to use a Mac, but probably don't make sense for most folks. :)
+
+Roughly, they set some really specific screen zoom features.
+
+## Applications
+
+## Safari:
+
+* Web Development tools. DevelopmentMenu, the more hidden extensions menu
+* Default views are favorites, about:blank as a home page.
+* Enables extensions, and shows the 1Password extension in my tool bar.
+* Don't open "safe" files after downloading (security risk!)
+* Use tab to navigate webpages
+* Personal Opinions:
+  * Cmd-1 to 9 for navigating to bookmarks
+  * Favicons in tabs
+  * ~/Desktop is the downloads location.
+  * Fewer top sites
+  * Don't autocleanup downloads
+
+### Termianl
+
+* Use my personal theme: Source Code Pro, large font.
+* Use bash...because habits.
+
+### Finder
+
+* Show hidden files
+* List view with calculate all sizes
+* Turn on statusbar, toolbar, sidebar.
+* Personal whacky stuff:
+  * Larger font size in list view
+  * Hide everything from the desktop.
+
+## TODO Domains to check about setting...
+
+These are my current list of `defaults domains` I intend to verify and selectively include.
+
+ 2BUA8C4S2C.com.agilebits.onepassword7-helper
+ Avatar Cache Index
+ ContextStoreAgent
+ MobileMeAccounts
+ ZoomChat
+ at.obdev.LaunchBar
+ com.Adobe.Common 14.0
+ com.Adobe.dynamiclinkmediaserver.13.0.0
+ com.adobe.Install
+ com.adobe.LightroomClassicCC7
+ com.adobe.Photoshop.uxp_com.adobe.ccx.lrimporter
+ com.adobe.Photoshop.uxp_com.adobe.ccx.start
+ com.adobe.acc.AdobeDesktopService
+ com.adobe.acc.HEXHelper.Renderer
+ com.adobe.accmac
+ com.adobe.crashreporter
+ com.agilebits.onepassword7
+ com.agilebits.onepassword7.1PasswordSafariAppExtension
+ com.allocinit.CamTwist
+ com.allocinit.CamTwist_Assistant
+ com.apple.AMPLibraryAgent
+ com.apple.Accessibility
+ com.apple.AccessibilityVisualsAgent
+ com.apple.ActivityMonitor
+ com.apple.AdLib
+ com.apple.AddressBook
+ com.apple.AppStore
+ com.apple.AppleMediaServices
+ com.apple.AppleMediaServices.notbackedup
+ com.apple.AppleMultitouchMouse
+ com.apple.AppleMultitouchTrackpad
+ com.apple.BKAgentService
+ com.apple.BezelServices
+ com.apple.CalendarAgent
+ com.apple.CalendarNotification.CalNCService
+ com.apple.CommCenter.counts
+ com.apple.CoreDuet
+ com.apple.CoreGraphics
+ com.apple.DictionaryServices
+ com.apple.DiskUtility
+ com.apple.EmojiCache
+ com.apple.EmojiPreferences
+ com.apple.ExpansionSlotUtility
+ com.apple.FaceTime
+ com.apple.FolderActionsDispatcher
+ com.apple.FontRegistry.user
+ com.apple.GEO
+ com.apple.HIToolbox
+ com.apple.Home
+ com.apple.Home.group
+ com.apple.Home.wallpaper
+ com.apple.Maps
+ com.apple.Messages
+ com.apple.Music
+ com.apple.NetworkBrowser
+ com.apple.Notes
+ com.apple.Photos
+ com.apple.Photos.Migration.Reader
+ com.apple.Preferences
+ com.apple.Preview
+ com.apple.Preview.ViewState
+ com.apple.ProblemReporter
+ com.apple.QuickTimePlayerX
+ com.apple.Safari.SandboxBroker
+ com.apple.SafariBookmarksSyncAgent
+ com.apple.ScreenTimeAgent
+ com.apple.ServicesMenu.Services
+ com.apple.SetupAssistant
+ com.apple.Siri
+ com.apple.Siri.SiriTodayExtension
+ com.apple.SiriNCService
+ com.apple.SocialPushAgent
+ com.apple.SoftwareUpdate
+ com.apple.SpeechRecognitionCore
+ com.apple.Spotlight
+ com.apple.SystemProfiler
+ com.apple.TTY
+ com.apple.TV
+ com.apple.TelephonyUtilities
+ com.apple.Terminal
+ com.apple.TextEdit
+ com.apple.TextInputMenu
+ com.apple.TextInputMenuAgent
+ com.apple.TimeMachine
+ com.apple.UIKit
+ com.apple.UserAccountUpdater
+ com.apple.VoiceOver
+ com.apple.VoiceOver4.local
+ com.apple.VoiceOverTraining
+ com.apple.VoiceOverUtility
+ com.apple.VoiceOverUtilityCacheBuilder
+ com.apple.accounts
+ com.apple.accountsd
+ com.apple.amp.mediasharingd
+ com.apple.appstored
+ com.apple.archiveutility
+ com.apple.assistant
+ com.apple.assistant.backedup
+ com.apple.assistant.support
+ com.apple.audio.AudioMIDISetup
+ com.apple.bird
+ com.apple.bookstoreagent
+ com.apple.calculateframework
+ com.apple.classroom
+ com.apple.cloudd
+ com.apple.cloudpaird
+ com.apple.cloudphotod
+ com.apple.cmfsyncagent
+ com.apple.commcenter
+ com.apple.commcenter.callservices
+ com.apple.commerce
+ com.apple.commerce.configurator
+ com.apple.commerce.knownclients
+ com.apple.contacts.donation-agent
+ com.apple.controlstrip
+ com.apple.coreauthd
+ com.apple.corerecents.recentsd
+ com.apple.coreservices.UASharedPasteboardProgressUI
+ com.apple.coreservices.uiagent
+ com.apple.coreservices.useractivityd
+ com.apple.coreservices.useractivityd.dynamicuseractivites
+ com.apple.corespotlightui
+ com.apple.desktopservices
+ com.apple.dock
+ com.apple.driver.AppleBluetoothMultitouch.mouse
+ com.apple.driver.AppleBluetoothMultitouch.trackpad
+ com.apple.driver.AppleHIDMouse
+ com.apple.dt.IDECacheDeleteAppExtension
+ com.apple.dt.xcodebuild
+ com.apple.facetime.bag
+ com.apple.fileproviderd
+ com.apple.finder
+ com.apple.frameworks.diskimages
+ com.apple.frameworks.diskimages.diuiagent
+ com.apple.gamecenter
+ com.apple.gamed
+ com.apple.help
+ com.apple.helpviewer
+ com.apple.homed
+ com.apple.homed.notbackedup
+ com.apple.iCal
+ com.apple.iChat
+ com.apple.iChat.inputLine
+ com.apple.iTunes
+ com.apple.iWork.Numbers
+ com.apple.icloud.fmfd
+ com.apple.icloud.fmip.clientconfiguration
+ com.apple.icloud.fmip.voiceassistantsync
+ com.apple.icloud.searchpartyuseragent
+ com.apple.identityservices.idstatuscache
+ com.apple.identityservicesd
+ com.apple.ids.deviceproperties
+ com.apple.ids.subservices
+ com.apple.imagecapture
+ com.apple.imagent
+ com.apple.imdpersistence.IMDPersistenceAgent
+ com.apple.imessage
+ com.apple.imessage.bag
+ com.apple.imservice.ids.FaceTime
+ com.apple.imservice.ids.iMessage
+ com.apple.internal.ck
+ com.apple.ipTelephony
+ com.apple.itunesstored
+ com.apple.keyboardservicesd
+ com.apple.keychainaccess
+ com.apple.locationmenu
+ com.apple.loginwindow
+ com.apple.madrid
+ com.apple.mail
+ com.apple.mediaaccessibility
+ com.apple.mediaanalysisd
+ com.apple.messages.nicknames
+ com.apple.mmcs
+ com.apple.motionapp
+ com.apple.ncplugin.stocks
+ com.apple.ncplugin.weather
+ com.apple.ncprefs
+ com.apple.networkresolutiond
+ com.apple.news
+ com.apple.newscore
+ com.apple.newscore2
+ com.apple.notificationcenterui
+ com.apple.parsecd
+ com.apple.passd
+ com.apple.photoanalysisd
+ com.apple.photolibraryd
+ com.apple.photos.shareddefaults
+ com.apple.preferences.extensions.ServicesWithUI
+ com.apple.preferences.extensions.ShareMenu
+ com.apple.preferences.softwareupdate
+ com.apple.print.PrintingPrefs
+ com.apple.print.custompresets.forprinter.Xerox_Phaser_6510__6c_d9_e0_
+ com.apple.proactive.PersonalizationPortrait
+ com.apple.protectedcloudstorage.protectedcloudkeysyncing
+ com.apple.quicklook.QuickLookUIService
+ com.apple.quicklook.ThumbnailsAgent
+ com.apple.rapport
+ com.apple.registration
+ com.apple.remindd
+ com.apple.remindd.babysitter
+ com.apple.reminders
+ com.apple.routined
+ com.apple.scheduler
+ com.apple.screencapture
+ com.apple.screensaver
+ com.apple.scriptmenu
+ com.apple.security.KCN
+ com.apple.security.cloudkeychainproxy3.keysToRegister
+ com.apple.security.ctkd-db
+ com.apple.security.sosaccount
+ com.apple.sharingd
+ com.apple.sidecar.display
+ com.apple.siri-distributed-evaluation
+ com.apple.siri.context.service
+ com.apple.siri.embeddedspeech
+ com.apple.siri.media-indexer
+ com.apple.sms
+ com.apple.spaces
+ com.apple.speech.recognition.AppleSpeechRecognition.CustomCommands
+ com.apple.speech.recognition.AppleSpeechRecognition.prefs
+ com.apple.speech.voice.prefs
+ com.apple.stockholm
+ com.apple.stocks
+ com.apple.storeagent
+ com.apple.studentd
+ com.apple.suggestions
+ com.apple.symbolichotkeys
+ com.apple.sync.NanoHome
+ com.apple.systempreferences
+ com.apple.systemuiserver
+ com.apple.talagent
+ com.apple.textInput.keyboardServices.textReplacement
+ com.apple.tourist
+ com.apple.touristd
+ com.apple.universalaccess
+ com.apple.universalaccessAuthWarning
+ com.apple.xpc.activity2
+ com.blackmagic-design.ATEM Setup
+ com.blackmagic-design.ATEM Software Control
+ com.blackmagic-design.Blackmagic Desktop Video Administration
+ com.blackmagic-design.DiskSpeedTest
+ com.blackmagic-design.desktopvideo.livekey
+ com.blackmagic-design.switchers.softwarecontrol
+ com.docker.docker
+ com.dropbox.DropboxMacUpdate
+ com.electron.docker-frontend
+ com.frontapp.Front
+ com.getdropbox.dropbox
+ com.google.Chrome
+ com.google.Chrome.canary
+ com.google.GoogleDrive
+ com.google.Keystone.Agent
+ com.hnc.Discord
+ com.macromates.TextMate.preview
+ com.microsoft.VSCode
+ com.primatelabs.Geekbench5
+ com.qtproject
+ com.smileonmymac.textexpander
+ com.tinyspeck.slackmacgap
+ com.twitter.twitter-mac
+ developer.apple.wwdc-Release
+ findInSelectionByDefault
+ info.eurocomp.Timing2
+ info.eurocomp.Timing2.fallback
+ info.eurocomp.TimingHelper
+ info.eurocomp.TimingHelper.InfoExtractorService
+ info.eurocomp.TimingHelper.fallback
+ knowledge-agent
+ loginwindow
+ org.cups.PrintingPrefs
+ org.m0k.transmission
+ pbs
+ systemgroup.com.apple.icloud.searchpartyd.sharedsettings
+ tv.sienna.scanconvertersdi
+ us.zoom.Transcode
+ us.zoom.xos
--- setup/osx.sh
+++ setup/osx.sh
@@ -71,12 +71,6 @@ defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 # (e.g. enable Tab in modal dialogs)
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
-# Enable access for assistive devices
-# echo -n 'a' | sudo tee /private/var/db/.AccessibilityAPIEnabled > /dev/null 2>&1
-# sudo chmod 444 /private/var/db/.AccessibilityAPIEnabled
-# NO LONGER WORKS IN 10.9
-
-# TODO Use only if Mac is a laptop
 # Automatically illuminate built-in MacBook keyboard in low light
 defaults write com.apple.BezelServices kDim -bool true
 # Turn off keyboard illumination when computer is not used for 3 minutes
@@ -108,7 +102,7 @@ sudo defaults write /Library/Preferences/com.apple.windowserver DisplayResolutio
 ###############################################################################
 # TextEdit   (Controls QuickLook Settings too...)                             #
 ###############################################################################
-defaults write com.apple.TextEdit NSFixedPitchFont -string "Helvetica"
+defaults write com.apple.TextEdit NSFixedPitchFont -string "Source Code Pro Light"
 defaults write com.apple.TextEdit NSFixedPitchFontSize -string "24"
 
 ###############################################################################
@@ -176,16 +170,10 @@ defaults write com.apple.frameworks.diskimages skip-verify -bool true
 defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
 defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
 
-# Automatically open a new Finder window when a disk image is mounted
-defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
-defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
-defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool false
 
 # Use list view in all Finder windows by default
 # Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
-
-defaults write com.apple.finder FXPreferredViewStyle -string "None"
 
 # New window location; must specify a full path.
 defaults write com.apple.finder NewWindowTargetPath -string "file:///Volumes/Michael/Users/Michael/Desktop/"
@@ -198,9 +186,6 @@ defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
 
 # hide tags in finder by default
 defaults write com.apple.finder SidebarTagsSctionDisclosedState -int 0
-
-# Enable the MacBook Air SuperDrive on any Mac
-sudo nvram boot-args="mbasd=1"
 
 # Show the ~/Library folder
 chflags nohidden ~/Library
@@ -224,24 +209,7 @@ defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
 # Show indicator lights for open applications in the Dock
 defaults write com.apple.dock show-process-indicators -bool true
 
-# Wipe all (default) app icons from the Dock
-defaults write com.apple.dock persistent-apps -array ""
-# Setup my dock
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Safari.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Drag and Drop Apps/Web/Google Chrome Canary.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Aperture.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Final Cut Pro.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/iTunes.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Calendar.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Reminders.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Messages.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Pages.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Drag and Drop Apps/Text/TextMate.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Xcode.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Utilities/Disk Utility.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/System Preferences.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Utilities/Activity Monitor.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Utilities/Terminal.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
+# TODO: Add applications to the dock?
 
 # Don’t animate opening applications from the Dock
 defaults write com.apple.dock launchanim -bool false
@@ -268,64 +236,9 @@ defaults write com.apple.dock showhidden -bool true
 # Safari & WebKit                                                             #
 ###############################################################################
 
-defaults write com.apple.Safari AlwaysShowTabBar -int 1
-
-# Set Safari’s home page to `about:blank` for faster loading
-defaults write com.apple.Safari HomePage -string "http://www.apple.com/startpage"
-
-# Prevent Safari from opening ‘safe’ files automatically after downloading
-defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
-
-# Disable hitting the Backspace key to go to the previous page in history
-defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled -bool false
-
-# Show Safari’s bookmarks bar by default
-defaults write com.apple.Safari ShowFavoritesBar -bool true
-
-# Disable Safari’s thumbnail cache for History and Top Sites
-defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
-
-# Enable Safari’s debug menu
-defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
-
-# Make Safari’s search banners default to Contains instead of Starts With
-defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false
-
-# Remove useless icons from Safari’s bookmarks bar
-defaults write com.apple.Safari ProxiesInBookmarksBar "()"
-
-# Enable the Develop menu and the Web Inspector in Safari
-defaults write com.apple.Safari IncludeDevelopMenu -bool true
-defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
-
-# Add a context menu item for showing the Web Inspector in web views
-defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
-
-###############################################################################
-# iTunes (pre-iTunes 11 only)                                                 #
-###############################################################################
-
-# Disable the iTunes store link arrows
-defaults write com.apple.iTunes show-store-link-arrows -bool false
-
-# Disable the Genius sidebar in iTunes
-defaults write com.apple.iTunes disableGeniusSidebar -bool true
-
-defaults write com.apple.iTunes allow-half-stars -boolean true
-defaults write com.apple.dock notification-always-show-image -boolean true
-defaults write com.apple.dock itunes-notifications -boolean true
-
-###############################################################################
-# Mail                                                                        #
-###############################################################################
-
-# Disable send and reply animations in Mail.app
-defaults write com.apple.mail DisableReplyAnimations -bool true
-defaults write com.apple.mail DisableSendAnimations -bool true
-
-# Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app
-defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
+# General Web Development Settings (inspectors, debug menus, etc)
+# Personal prefs: Icons in tabs, 1pw button, save to ~/Desktop
+defaults import -app Safari setup/app-preferences/safari.plist
 
 ###############################################################################
 # Terminal                                                                    #
@@ -377,56 +290,6 @@ defaults write com.apple.appstore WebKitDeveloperExtras -bool true
 defaults write com.apple.appstore ShowDebugMenu -bool true
 
 ###############################################################################
-# Transmission.app                                                            #
-###############################################################################
-# TODO --make sure this works.
-# Use `~/Desktop` to store  downloads
-defaults write org.m0k.transmission DownloadFolder -string "${HOME}/Desktop/"
-
-# Don’t prompt for confirmation before downloading
-defaults write org.m0k.transmission DownloadAsk -bool false
-
-# Trash original torrent files
-defaults write org.m0k.transmission DeleteOriginalTorrent -bool true
-
-# Hide the donate message
-defaults write org.m0k.transmission WarningDonate -bool false
-# Hide the legal disclaimer
-defaults write org.m0k.transmission WarningLegal -bool false
-
-###############################################################################
-# Twitter.app                                                                 #
-###############################################################################
-
-# Show the app window when clicking the menu icon
-defaults write com.twitter.twitter-mac MenuItemBehavior -int 1
-
-# Enable the hidden ‘Develop’ menu
-defaults write com.twitter.twitter-mac ShowDevelopMenu -bool true
-
-# Open links in the background
-defaults write com.twitter.twitter-mac openLinksInBackground -bool true
-
-# Allow closing the ‘new tweet’ window by pressing `Esc`
-defaults write com.twitter.twitter-mac ESCClosesComposeWindow -bool true
-
-# Show full names rather than Twitter handles
-defaults write com.twitter.twitter-mac ShowFullNames -bool true
-
-# Hide the app in the background if it’s not the front-most window
-defaults write com.twitter.twitter-mac HideInBackground -bool true
-
-###############################################################################
-# TextMate                                                 #
-###############################################################################
-# Make using the Find dialog to defualt to selected text
-defaults write findInSelectionByDefault -bool YES
-# Turn off font smoothing for all displays on dark themes
-defaults write com.macromates.TextMate.preview fontSmoothing 2
-# Selecting items in file finder will loop around
-defaults write com.macromates.TextMate.preview enableLoopFilterList --bool YES
-
-###############################################################################
 # Kill affected applications                                                  #
 ###############################################################################
 
@@ -436,5 +299,3 @@ for app in "Address Book" "Calendar" "Contacts" "Dashboard" "Dock" "Finder" \
 	killall "$app" > /dev/null 2>&1
 done
 echo "Done. Note that some of these changes require a logout/restart to take effect."
-
-
