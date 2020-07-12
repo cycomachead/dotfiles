19d5aaf50634b0a69e474fe9f56fd2346a5b7bc1
Michael Ball <cycomachead@gmail.com>
2020-07-12 16:29:52 -0700

    Some cleanup

--- setup/link_home_files.sh
+++ setup/link_home_files.sh
@@ -1,15 +1,24 @@
 #! /usr/bin/env bash
 
-dotfiles="$HOME/Dropbox/Projects/dotfiles/home/"
+# Creates a symlink to all the files in home/
+# Uses ~/ to be independent of home folder/drive path
+# (incase it changes after the script has been run.)
+linkPath='Dropbox/Projects/dotfiles/home/';
+# Otherwise, can't correctly loop over ~/...
+# dotfiles=$(printf %q "$HOME/$linkPath")
+dotfiles=$HOME/$linkPath;
 
 function symlink {
     echo "Linking $1"
     rm .$1;
-    ln -s $dotfiles$1 .$1;
+    ln -s "$dotfiles$1" .$1;
 }
 
 pushd ~/;
-for file in $(ls $dotfiles)
+echo "Linking files in $dotfiles";
+# files=`ls '/Volumes/Michael 1/Users/michael/Dropbox/Projects/dotfiles/home'`;
+files=`ls "$dotfiles"`;
+for file in $files
 do
     symlink $file;
 done
