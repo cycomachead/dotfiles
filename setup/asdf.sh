79b4b283721eed788e9685601c31e4b8aaa92068
Michael Ball <michael@mball.co>
2023-07-08 04:41:34 -0700

    try using asdf now insteas of nvm/rvm

---
+++ setup/asdf.sh
@@ -0,0 +1,8 @@
+# Install and setup asdf
+
+brew install asdf
+
+asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+
+asdf install nodejs 14.21.3
