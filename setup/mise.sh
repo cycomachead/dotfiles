380cf2546bb6c2ccbdd95d1c990702c64b303878
Michael Ball <michael@mball.co>
2025-04-17 14:23:20 -0700

    Switch from asdf to mise

---
+++ setup/mise.sh
@@ -0,0 +1,17 @@
+# Install and setup mise
+
+# This is probably a no-op, but just for convenience.
+brew install mise
+
+mise install ruby 3.3
+mise install ruby 3.3.7
+mise install ruby 3.2.2
+mise install node@20
+
+# Some projects still need node 16.x
+mise install node@16
+
+# Python
+mise install python
+mise install python@3.10
+mise install python@3.9
